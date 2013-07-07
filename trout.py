aa0b4b074dbc00a864342f8ef976fb7d90bd8d98
johnafish <john@johnafish.ca>
2013-07-07 19:38:00 -0400

    Site generation nicer.

--- trout.py
+++ trout.py
@@ -1,13 +1,14 @@
-#Trout, written by John Fish. 
-
+#Trout, written by John Fish in July 2013.
 import os
+#Import headers and footer. Each are seperate text files containing within them the code for the half of the code before and after their respective sides of the article. articleList is the list of written articles (simple enough!)
+headerOne = open('headerOne', 'r+')
+headerTwo = open('headerTwo', 'r+')
+footer = open('footer', 'r+')
+articleList = open('articleList', 'r+')
 
 fileName = input("What is the file name?")
 
-header = open('header', 'r+')
-footer = open('footer', 'r+')
+article = open('articles/'+fileName, 'r+')
+articleHTML = open('htmlArticles/'+fileName+'.html', 'w')
 
-testfile = open('articles/testfile', 'r+')
-testhtml = open('htmlArticles/testfile.html', 'w')
-testhtml.write(header.read()+'<p>This worked dandily!</p>'+footer.read())
-print(testfile.readline())
+articleHTML.write(headerOne.read()+article.readline()+headerTwo.read()+'<p>'+article.read()+'</p>'+footer.read())
